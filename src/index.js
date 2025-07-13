19b5462f8304fffb5e3ab2e99b445ca8f8065ae0
Karabo-L <karabolesapo2@gmail.com>
2025-07-13 23:21:29 +0200

    added AI API intergration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "POEM POEM POEM POEM POEM POEM",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let inputText = document.querySelector("#intext");
+  let prompt = `Generate a poem about ${inputText.value}`;
+  let context =
+    "You are a poem expect and love to write short poems. Your mission is to write a 4 line poemin basic HTML and separate each line with a <br/> . Make sure to follow the user instructions and sign the poem with your ai signature";
+  let apiKey = "oca241dc50fc43eft2aa6936faa5a0b1";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#poem-gen");
