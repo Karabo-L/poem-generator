b3502d704e0e27a4f6fabaf5ad1f0b94c2ee445a
Karabo-L <karabolesapo2@gmail.com>
2025-07-13 22:20:37 +0200

    Added basic form handling and added typewritter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "POEM POEM POEM POEM POEM POEM",
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let poemForm = document.querySelector("#poem-gen");
+
+poemForm.addEventListener("submit", generatePoem);
