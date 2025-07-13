a0c9046ed4cde8585ed95506e30bb8e0e1db783d
Karabo-L <karabolesapo2@gmail.com>
2025-07-13 23:54:56 +0200

    improved the context

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generatePoem(event) {
   let inputText = document.querySelector("#intext");
   let prompt = `Generate a poem about ${inputText.value}`;
   let context =
-    "You are a poem expect and love to write short poems. Your mission is to write a 4 line poemin basic HTML and separate each line with a <br/> . Make sure to follow the user instructions and sign the poem with your ai signature";
+    "You are a poem expect and love to write short poems. Your mission is to write a 4 line poem and separate each line with a <br/> . Make sure to follow the user instructions and sign the poem with your ai signature";
   let apiKey = "oca241dc50fc43eft2aa6936faa5a0b1";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
